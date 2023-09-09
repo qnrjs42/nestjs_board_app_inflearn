6d5bb9768e211479db63aaf7648bfa86da6b72a5
qnrjs42 <qnrjs42@naver.com>
2023-09-09 15:45:57 +0900

    #3-1. 모든 게시물을 가져오는 Service 만들기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,11 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
-  boardsService: BoardsService;
+  constructor(private boardsService: BoardsService) {}
 
-  constructor(boardsService: BoardsService) {
-    this.boardsService = boardsService;
+  @Get()
+  getAllBoards() {
+    return this.boardsService.getAllBoards();
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boards = ['asd', 'sdf', 'dfg']; // 다른 곳에서 접근할 수 없도록 private로 선언
+
+  getAllBoards() {
+    return this.boards;
+  }
+}
