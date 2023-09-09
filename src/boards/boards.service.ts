d7a277f62d2e2bc0c442d99358f5f02261bfbcfc
qnrjs42 <qnrjs42@naver.com>
2023-09-09 15:56:59 +0900

    #3-2. Board Model 정의하기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './boards.model';
 
 @Injectable()
 export class BoardsService {
-  private boards = ['asd', 'sdf', 'dfg']; // 다른 곳에서 접근할 수 없도록 private로 선언
+  private boards: Board[] = []; // 다른 곳에서 접근할 수 없도록 private로 선언
 
   getAllBoards() {
     return this.boards;
