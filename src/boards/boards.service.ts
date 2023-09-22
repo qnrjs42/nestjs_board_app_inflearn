6f97f806e49722804ab9c99dc4805b98d27ca996
qnrjs42 <qnrjs42@naver.com>
2023-09-22 14:02:46 +0900

    #6-1. 데이터베이스를 위한 소스코드정리

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,43 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './boards.model';
-import { nanoid } from 'nanoid';
-import { CreateBoardDto } from './dto/create-board.dto';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {
-  private boards: Board[] = []; // 다른 곳에서 접근할 수 없도록 private로 선언
-
-  getAllBoards(): Board[] {
-    return this.boards;
-  }
-
-  createBoard({ title, description }: CreateBoardDto): Board {
-    const board: Board = {
-      id: nanoid(),
-      title,
-      description,
-      status: 'PUBLIC',
-    };
-    this.boards.push(board);
-    return board;
-  }
-
-  getBoardById(id: string): Board {
-    const found = this.boards.find(board => board.id === id);
-
-    if (!found) throw new NotFoundException(`Can't find Board with id ${id}`);
-
-    return found;
-  }
-
-  updateBoardStatus(id: string, status: BoardStatus): Board {
-    const board = this.getBoardById(id);
-    board.status = status;
-    return board;
-  }
-
-  deleteBoard(id: string): void {
-    const found = this.getBoardById(id);
-    this.boards = this.boards.filter(board => board.id !== found.id);
-  }
-}
+export class BoardsService {}
