0f3c67fa08fd53aaa8d1efc9101d24802c8cc6a6
qnrjs42 <qnrjs42@naver.com>
2023-09-22 14:14:01 +0900

    #6-2. ID를 이용해서 특정 게시물 가져오기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entity';
+import { BoardRepository } from './board.repository';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  constructor(
+    @InjectRepository(BoardRepository)
+    private boardRepository: BoardRepository,
+  ) {}
+
+  async getBoardById(id: number): Promise<Board> {
+    const found = await this.boardRepository.findOne(id);
+
+    if (!found) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+
+    return found;
+  }
+}
